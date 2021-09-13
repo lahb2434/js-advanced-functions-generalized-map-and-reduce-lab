4e95341a271a314d825a863d3e2a622491574460
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-09-12 21:36:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Add your functions here
+function mapToNegativize(sourceArray, func) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(func(sourceArray[i]))
+    }
+    return newArray
+}
